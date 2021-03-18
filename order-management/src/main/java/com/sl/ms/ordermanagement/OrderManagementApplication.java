4fa867424446123765b9de665338f75cc3779b98
ssrikanth1000 <80350211+ssrikanth1000@users.noreply.github.com>
2021-03-18 13:15:18 +0530

    Add files via upload

--- order-management/src/main/java/com/sl/ms/ordermanagement/OrderManagementApplication.java
+++ order-management/src/main/java/com/sl/ms/ordermanagement/OrderManagementApplication.java
@@ -2,8 +2,12 @@ package com.sl.ms.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableCaching
 public class OrderManagementApplication {
 
 	public static void main(String[] args) {
